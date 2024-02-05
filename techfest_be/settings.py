d39846b5ef8a4baec66a2270c7b49669561489ea
Karl Devlin Chau <122105152+devlinchau@users.noreply.github.com>
2024-02-06 03:17:21 +0800

    fix: add credentials allow

--- techfest_be/settings.py
+++ techfest_be/settings.py
@@ -54,9 +54,9 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Adjust this to match your React app's origin
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'techfest_be.urls'
 
